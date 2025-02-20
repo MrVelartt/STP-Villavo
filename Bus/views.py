64e3310e1da5c3b8a1c1193a81b5d3f1e4cae80e
J Camilo Velasquez <jcvelasquez@minambiente.gov.co>
2025-02-20 17:03:09 -0500

    Se suben 3 servicios basicos

--- Bus/views.py
+++ Bus/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import AppInfo, Ruta, DetalleRuta
+from .serializers import AppInfoSerializer, RutaSerializer, DetalleRutaSerializer
 
-# Create your views here.
+class AppInfoViewSet(viewsets.ModelViewSet):
+    queryset = AppInfo.objects.all()
+    serializer_class = AppInfoSerializer
+
+class RutaViewSet(viewsets.ModelViewSet):
+    queryset = Ruta.objects.all()
+    serializer_class = RutaSerializer
+
+class DetalleRutaViewSet(viewsets.ModelViewSet):
+    queryset = DetalleRuta.objects.all()
+    serializer_class = DetalleRutaSerializer
