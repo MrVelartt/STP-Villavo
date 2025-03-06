78c4fd58a74bfe1fbf689b7a295ca27ce19f7468
MrVelartt <velarttdesign@gmail.com>
2025-03-06 18:11:35 -0500

    Se configura database PGSQL

--- Bus/models.py
+++ Bus/models.py
@@ -1,30 +1,62 @@
 from django.db import models
+from django.utils import timezone
+from django.core.exceptions import ValidationError
+from django.core.files.images import get_image_dimensions
+from PIL import Image
 
 class CaracteristicaApp(models.Model):
-    icono = models.CharField(max_length=255)
-    nombre_caracteristica = models.CharField(max_length=255)
-    descripcion_caracteristica = models.TextField()
+    icono = models.ImageField(upload_to='iconos/', null=True, blank=True, verbose_name="Ícono")
+    nombre_caracteristica = models.CharField(max_length=255, verbose_name="Nombre de la característica")
+    descripcion_caracteristica = models.TextField(verbose_name="Descripción de la característica")
+
+    def __str__(self):
+        return self.nombre_caracteristica
+
+    class Meta:
+        verbose_name = "Característica de la App"
+        verbose_name_plural = "Características de la App"
 
 class AppInfo(models.Model):
-    icono_app = models.CharField(max_length=255)
-    nombre_app = models.CharField(max_length=255)
-    descripcion_app = models.TextField()
-    caracteristicas_app = models.ManyToManyField(CaracteristicaApp)
+    icono_app = models.ImageField(upload_to='iconos/', null=True, blank=True, verbose_name="Ícono de la App")
+    nombre_app = models.CharField(max_length=255, verbose_name="Nombre de la App")
+    descripcion_app = models.TextField(verbose_name="Descripción de la App")
+    caracteristicas_app = models.ManyToManyField(CaracteristicaApp, verbose_name="Características de la App")
+
+    def __str__(self):
+        return self.nombre_app
+
+    class Meta:
+        verbose_name = "Información de la App"
+        verbose_name_plural = "Información de la App"
 
 class Ruta(models.Model):
-    name_route = models.CharField(max_length=255)
-    frequency_route = models.IntegerField()
-    description_route = models.TextField()
-    color_route = models.CharField(max_length=7)  # Código hexadecimal
+    name_route = models.CharField(max_length=255, verbose_name="Nombre de la Ruta")
+    frequency_route = models.IntegerField(verbose_name="Frecuencia de la Ruta (min)")
+    description_route = models.TextField(verbose_name="Descripción de la Ruta")
+    color_route = models.CharField(max_length=7, verbose_name="Color de la Ruta (Hexadecimal)")
+
+    def __str__(self):
+        return self.name_route
+
+    class Meta:
+        verbose_name = "Ruta"
+        verbose_name_plural = "Rutas"
 
 class DetalleRuta(models.Model):
-    image_route = models.CharField(max_length=255)
-    name_route = models.CharField(max_length=255)
-    distance_route = models.CharField(max_length=100)
-    time_travel = models.IntegerField()
-    start_time_route = models.DateTimeField()
-    end_time_route = models.DateTimeField()
-    frequency = models.IntegerField()
-    quantity_bus = models.IntegerField()
-    description_route = models.TextField()
-    barrios = models.JSONField()  # Lista de nombres de barrios
+    image_route = models.ImageField(upload_to='rutas/', null=True, blank=True, verbose_name="Imagen de la Ruta")
+    name_route = models.CharField(max_length=255, verbose_name="Nombre de la Ruta")
+    distance_route = models.CharField(max_length=100, verbose_name="Distancia de la Ruta")
+    time_travel = models.IntegerField(verbose_name="Tiempo Estimado de Viaje (min)")
+    start_time_route = models.DateTimeField(default=timezone.now, verbose_name="Hora de Inicio")
+    end_time_route = models.DateTimeField(verbose_name="Hora de Finalización")
+    frequency = models.IntegerField(verbose_name="Frecuencia (min)")
+    quantity_bus = models.IntegerField(verbose_name="Cantidad de Buses en la Ruta")
+    description_route = models.TextField(verbose_name="Descripción Detallada")
+    barrios = models.JSONField(null=True, blank=True,verbose_name="Lista de Barrios en la Ruta")
+
+    def __str__(self):
+        return f"{self.name_route} - {self.distance_route}"
+
+    class Meta:
+        verbose_name = "Detalle de Ruta"
+        verbose_name_plural = "Detalles de Rutas"
