2c54aac4fb00818ccd77baac990393ed4ed06c6b
MrVelartt <velarttdesign@gmail.com>
2025-03-10 19:18:06 -0500

    Se agrega authentication jwt

--- Bus/views.py
+++ Bus/views.py
@@ -1,23 +1,29 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import AppInfo, CaracteristicaApp, Ruta, DetalleRuta
 from .serializers import AppInfoSerializer, CaracteristicaAppSerializer, RutaSerializer, DetalleRutaSerializer
 
-class AppInfoViewSet(viewsets.ReadOnlyModelViewSet):
+class AppInfoViewSet(viewsets.ModelViewSet): 
     """API para obtener información de la aplicación"""
     queryset = AppInfo.objects.all()
     serializer_class = AppInfoSerializer
+    permission_classes = [IsAuthenticated]  # Requiere autenticación
 
-class CaracteristicaAppViewSet(viewsets.ReadOnlyModelViewSet):
+class CaracteristicaAppViewSet(viewsets.ModelViewSet): 
     """API para obtener las características de la aplicación"""
     queryset = CaracteristicaApp.objects.all()
     serializer_class = CaracteristicaAppSerializer
+    permission_classes = [IsAuthenticated]  # Requiere autenticación
 
-class RutaViewSet(viewsets.ReadOnlyModelViewSet):
+class RutaViewSet(viewsets.ModelViewSet): 
     """API para obtener las rutas disponibles"""
     queryset = Ruta.objects.all()
     serializer_class = RutaSerializer
+    permission_classes = [IsAuthenticated]  # Requiere autenticación
 
 class DetalleRutaViewSet(viewsets.ReadOnlyModelViewSet):
-    """API para obtener los detalles de las rutas"""
+    """API para obtener detalles de las rutas y permitir login"""
     queryset = DetalleRuta.objects.all()
     serializer_class = DetalleRutaSerializer
+    #permission_classes = [IsAuthenticated]
+
