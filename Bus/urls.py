ebc6f1cec7245ffb8b5805128f319334b52c7775
MrVelartt <velarttdesign@gmail.com>
2025-03-10 19:18:06 -0500

    Se agrega authentication jwt

--- Bus/urls.py
+++ Bus/urls.py
@@ -1,13 +1,25 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import AppInfoViewSet, CaracteristicaAppViewSet, RutaViewSet, DetalleRutaViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import (
+    AppInfoViewSet, 
+    CaracteristicaAppViewSet, 
+    RutaViewSet, 
+    DetalleRutaViewSet
+)
 
+# Configuración del enrutador para las vistas basadas en ViewSets
 router = DefaultRouter()
-router.register(r'app-info', AppInfoViewSet)
-router.register(r'caracteristicas', CaracteristicaAppViewSet)
-router.register(r'rutas', RutaViewSet)
-router.register(r'detalle-rutas', DetalleRutaViewSet)
+router.register(r'app-info', AppInfoViewSet, basename="app-info")
+router.register(r'caracteristicas', CaracteristicaAppViewSet, basename="caracteristicas")
+router.register(r'rutas', RutaViewSet, basename="rutas")
+router.register(r'detalle-rutas', DetalleRutaViewSet, basename="detalle-rutas")
 
 urlpatterns = [
+    # Rutas de la API principal
     path('api/', include(router.urls)),
+
+    # Rutas de autenticación con JWT
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
